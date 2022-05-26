ded115d1f0af1efe460be132631cb34f7b51d1d9
Marwa Omar <farabi.marwa@gmail.com>
2022-05-27 01:55:04 +0200

    add more ml algorithms

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ import nltk # preprocessing
 #for model-building
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
+from sklearn import preprocessing
 
 #for word embedding
 from gensim.models import Word2Vec
@@ -52,7 +55,7 @@ def transform_w2v_to_vector(word2vec, vector_size, X):
 dictionary = []
 with open('data.json', mode = 'r', errors = 'ignore') as json_file:
     for dic in json_file:
-        dictionary.append( json.loads(dic) )
+        dictionary.append(json.loads(dic))
 
 
 # create dtf
@@ -67,12 +70,16 @@ dataSet["headline_clean"] = dataSet["headline"].apply(lambda x: process_text(x))
 # split dataset
 x_train, x_test, y_train, y_test = train_test_split(dataSet["headline_clean"],dataSet["category"],test_size=0.2,shuffle=True)
 
+# label encode the target variable
+# encoder = preprocessing.LabelEncoder()
+# y_train = encoder.fit_transform(y_train)
+# y_test = encoder.fit_transform(y_test)
 
 # Word embedding
 dataSet["headline_clean_tokenized"] = [nltk.word_tokenize(i) for i in dataSet["headline_clean"]]
 
-vector_size = 300
-model = Word2Vec(dataSet["headline_clean_tokenized"], vector_size=vector_size, window=8, min_count=5, sg=1, workers=30)
+vector_size = 100
+model = Word2Vec(dataSet["headline_clean_tokenized"], vector_size=vector_size, window=8, min_count=5, sg=1, workers=10, epochs=8)
 w2v = dict(zip(model.wv.index_to_key, model.wv.vectors))
 
 x_train_tokenized = [nltk.word_tokenize(i) for i in x_train]
@@ -82,10 +89,32 @@ x_train_vectors_w2v = transform_w2v_to_vector(w2v, vector_size, x_train_tokenize
 x_test_vectors_w2v = transform_w2v_to_vector(w2v, vector_size, x_test_tokenized)
 
 
-# Fitting the classification model using Logistic Regression
-lr_w2v = LogisticRegression(solver='liblinear', C=10, penalty='l2')
-lr_w2v.fit(x_train_vectors_w2v, y_train)  # model
-
+# Fitting the classification model using logistic regression
+print('\nLogistic Regression')
+lr_model = LogisticRegression(solver='sag', C=8, multi_class='multinomial')
+lr_model.fit(x_train_vectors_w2v, y_train)
 # Predict y value for test dataset
-y_predict = lr_w2v.predict(x_test_vectors_w2v)
-print(classification_report(y_test, y_predict))
+y_predict_lr = lr_model.predict(x_test_vectors_w2v)
+print(classification_report(y_test, y_predict_lr))
+print('\n------------------------------------------\n')
+
+
+# Fitting the classification model using svm
+print('SVM')
+svm_model = LinearSVC()
+svm_model.fit(x_train_vectors_w2v, y_train)
+# Predict y value for test dataset
+y_predict_svm = svm_model.predict(x_test_vectors_w2v)
+print(classification_report(y_test, y_predict_svm))
+print('\n------------------------------------------\n')
+
+
+# Fitting the classification model using random forrest
+print('Random Forrest')
+rf_model = RandomForestClassifier()
+rf_model.fit(x_train_vectors_w2v, y_train)
+# Predict y value for test dataset
+y_predict_rf = rf_model.predict(x_test_vectors_w2v)
+print(classification_report(y_test, y_predict_rf))
+
+
